bc40227fd06f601ff2321a56c055def324615189
Lamskidoski <lam.ky@hotmail.com>
2024-02-28 00:47:00 +0100

    popup screen

--- Script.js
+++ Script.js
@@ -1,3 +1,16 @@
+const openBtn = document.getElementById("modal")
+const closeBtn = document.getElementById("close-button")
+const mercury = document.getElementById("mercury")
+const venus = document.getElementById("venus")
+const earth = document.getElementById("earth")
+const mars = document.getElementById("mars")
+const jupiter = document.getElementById("jupiter")
+const saturn = document.getElementById("saturn")
+const uranus = document.getElementById("uranus")
+const neptune = document.getElementById("neptune")
+
+
+
 function getBodies() {
   const apiUrl = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies';
   const apiKey = 'solaris-2ngXkR6S02ijFrTP';
@@ -8,15 +21,107 @@ function getBodies() {
   })
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    console.log(data);
+    let planet = data.bodies;
+    console.log(planet[3].name);
+    mercury.addEventListener("click", function () {
+      getInfo(planet[1]);
+      getStatus(planet[1]);
+      getMoons(planet[1]);
+      openModal();
+    })
+    venus.addEventListener("click", function () {
+      getInfo(planet[2]);
+      getStatus(planet[2]);
+      getMoons(planet[2]);
+      openModal();
+    })
+    earth.addEventListener("click", function () {
+      getInfo(planet[3]);
+      getStatus(planet[3]);
+      getMoons(planet[3]);
+      openModal();
+    })
+    mars.addEventListener("click", function () {
+      getInfo(planet[4]);
+      getStatus(planet[4]);
+      getMoons(planet[4]);
+      openModal();
+    })
+    jupiter.addEventListener("click", function () {
+      getInfo(planet[5]);
+      getStatus(planet[5]);
+      getMoons(planet[5]);
+      openModal();
+    })
+    saturn.addEventListener("click", function () {
+      getInfo(planet[6]);
+      getStatus(planet[6]);
+      getMoons(planet[6]);
+      openModal();
+    })
+    uranus.addEventListener("click", function () {
+      getInfo(planet[7]);
+      getStatus(planet[7]);
+      getMoons(planet[7]);
+      openModal();
+    })
+    neptune.addEventListener("click", function () {
+      getInfo(planet[8]);
+      getStatus(planet[8]);
+      getMoons(planet[8]);
+      openModal();
+    })
+    
+
     // Hantera den mottagna datan här
   })
   .catch(error => {
     console.error('Det uppstod ett fel:', error);
   });
 }
-
 getBodies();
+
+// öppnar med hjälp av knappen
+function openModal() {
+  openBtn.style.display = "block";
+  console.log("du har öppnat");
+}
+
+closeBtn.addEventListener("click", function() {
+  openBtn.style.display = "none"
+})
+
+function getInfo(planetData) {
+  const title = document.getElementById("title")
+  title.innerHTML = `
+  <h1 style="color: white;">${planetData.name}</h1>
+  <h2 style="color: Yellow;">${planetData.latinName}</h2>
+  <p style="color: white;">${planetData.desc}</p>
+  `
+}
+
+function getStatus(planetStatus) {
+  const status = document.getElementById("status")
+  status.innerHTML = `
+  <h2 style="color: yellow;">Omkrets</h2>
+  <p style="color: white;">${planetStatus.circumference}</p>
+  <h2 style="color: yellow;">KM från solen</h2>
+  <p style="color: white;">${planetStatus.distance}</p>
+  <h2 style="color: yellow;">Tepmeratur</h2>
+  <p style="color: white;">${planetStatus.temp}</p>
+  `
+}
+
+function getMoons (planetMoons) {
+  const moons = document.getElementById("moons")
+  moons.innerHTML = `
+  <h2 style="color: yellow;">Månar</h2>
+  <p style="color: white;">${planetMoons.moons}</p>
+  `
+}
+
+
 
 const SKY = document.querySelector('[data-sky]');
 // quantity stars per 100x100px 
@@ -48,8 +153,8 @@ if (SKY) {
     SKY.innerHTML = starTemplate;
   }
   function getStarsQuantitiy(skySize) {
-    const qH = skySize.width / 500 * (QUANTITY / 2 );
-    const qV = skySize.heigt / 500 * (QUANTITY / 2 );
+    const qH = skySize.width / 600 * (QUANTITY / 2 );
+    const qV = skySize.heigt / 600 * (QUANTITY / 2 );
     return qH + qV;
   }
   function getStarPos(skySize) {
@@ -60,39 +165,51 @@ if (SKY) {
   }
 }
 
-const mercury = document.querySelector(".mercury")
-const venus = document.getElementsByClassName("venus")
-const earth = document.getElementsByClassName("earth")
-const mars = document.getElementsByClassName("mars")
-const jupiter = document.getElementsByClassName("jupiter")
-const saturn = document.getElementsByClassName("saturn")
-const uranus = document.getElementsByClassName("uranus")
-const neptune = document.getElementsByClassName("neptune")
 
 
 
-mercury.addEventListener("click", function() {
-planetInfo(fetchData.bodies[1])
-})
 
-function planetInfo(planetData) {
-  const popUp = document.getElementsByClassName("popUp");
 
-  popUp.innerHTML = `
-  <h1>${planetData.name}</h1>
-  <h2>${planetData.latinName}</h2>
-  <p>${planetData.desc}</p>
-  <h3>Omkrets</h3>
-  <p>${planetData.circumference}</p>
-  <h4>KM från solen</h4>
-  <p>${planetData.distance}</p>
-  <h5>Tepmeratur</h5>
-  <p>${planetData.temp}</p>
-  <h6>Månar</h6>
-  <p>${planetData.moons}</p>
-  `
-}
+// mercury.addEventListener("click", function() {
+// planetInfo(fetchData.bodies[1])
+// })
+
+
 
 // visar popUp när den är fylld med information
 // popUp.style.display = "block";
 // popUp.style.position = "absolute";
+
+
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById("overlay")
+
+// openModalButtons.forEach(button => {
+// button.addEventListener("click", () => {
+//   const modal = document.querySelector(button.dataset.modalTarget)
+//   openModal(modal)
+// })
+// })
+
+// closeModalButtons.forEach(button => {
+//   button.addEventListener("click", () => {
+//     const modal = button.closest("modal")
+//     closeModal(modal)
+//   })
+//   })
+
+// function openModal(modal) {
+//   if (modal == null) return
+//   popup.classList.add("active")
+//   overlay.classList.add("active")
+// }
+
+// function closeModal(modal) {
+//   if (modal == null) return
+//   popup.classList.remove("active")
+//   overlay.classList.remove("active")
+// }
+
+
+
